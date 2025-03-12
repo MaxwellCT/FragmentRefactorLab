6df704680f61df272e5a4a22beab483614c1e02e
Centrous <maxwellchanetekimang@gmail.com>
2025-03-12 16:25:54 -0400

    made new fragment for the layout so that the code when it runs won't be in the main

---
+++ app/src/main/java/edu/temple/fragmentrefactor/BlankFragment.kt
@@ -0,0 +1,32 @@
+package edu.temple.fragmentrefactor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class BlankFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false).apply {
+            val displayTextView = findViewById<TextView>(R.id.displayTextView)
+            val nameEditText = findViewById<EditText>(R.id.nameEditText)
+
+            findViewById<Button>(R.id.Button).setOnClickListener {
+                val name = nameEditText.text
+
+                displayTextView.text = if (name.isNotBlank()) {
+                    "Hello, $name!"
+                } else {
+                    "Please enter your name"
+                }
+            }
+        }
+    }
+}
